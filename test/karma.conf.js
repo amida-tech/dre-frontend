b74a430e22ba7455cc515c63383d35b654c5797d
Geonixer <Geonixer@gmail.com>
2015-08-11 16:53:53 +0300

    pull
    Signed-off-by: Geonixer <Geonixer@gmail.com>

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -32,6 +32,7 @@ module.exports = function(config) {
       'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
       'bower_components/lodash/lodash.js',
       'bower_components/lodash-angular-wrapper/lodash-angular-wrapper.js',
+      'bower_components/restangular/dist/restangular.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
