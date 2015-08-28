25d6c85495949768e97e79d776cea03c73ef30a8
Igor Ivanov <iivanov@sinergo.ru>
2015-08-28 15:33:45 +0600

    imlemented Medications model, changes in FHIR-service & demo controller

--- app/dre-frontend.fhir/scripts/services/dre-frontend-medications.js
+++ app/dre-frontend.fhir/scripts/services/dre-frontend-medications.js
@@ -1,35 +1,37 @@
 "use strict";
 
 angular.module('dreFrontend.fhir')
-  .factory('DreFrontendMedications',function(dreFrontendFhirService){
-
+  .factory('DreFrontendMedications', function (dreFrontendFhirService) {
 
     function Medications(data) {
       this.setData(data);
     }
 
-    Medications.prototype.setData = function(data) {
+    Medications.prototype.setData = function (data) {
       if (data)
         angular.extend(this, data);
     };
 
     var medications = {
-      getByPatientId: function(patient_id) {
+      getByPatientId: function (patient_id) {
         return dreFrontendFhirService.search("MedicationPrescription", {patient: patient_id})
           .then(function (response) {
-            /* do data formating here */
-            _.each(response.enrty,function(resource){
+            angular.forEach(response.entry, function (resource) {
               resource.medication.load();
             });
             return new Medications(response);
-        });
+          });
       },
-      getOne: function(id) {
-        return dreFrontendFhirService.read('Medication',id)
-          .then(function(response){
-            console.log(response);
-            new Medications(response);
-            return response;
+      getById: function (id) {
+        return dreFrontendFhirService.read('Medication', id)
+          .then(function (response) {
+            return new Medications(response);
+          });
+      },
+      getAll: function () {
+        return dreFrontendFhirService.read('Medication')
+          .then(function (response) {
+            return new Medications(response);
           });
       }
     };
