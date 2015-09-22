6b8accdc07c4dd22ee414e51aad8b5c4a09d9059
TheMacros <TheMacros@mail.ru>
2015-09-22 23:28:42 +0300

    procedures

---
+++ app/scripts/controllers/procedures.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dreFrontendApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the dreFrontendApp
+ */
+angular.module('dreFrontendApp')
+    .controller('ProceduresCtrl', function ($scope, dreFrontendProcedures, _, dreFrontEndPatientInfo, dreFrontendUtil) {
+        $scope.model = {
+            userName: '-',
+            lastUpdate: new Date(),
+            proceduresList: []
+        };
+        dreFrontEndPatientInfo.getPatientData().then(function (patient) {
+            $scope.model.userName = patient.getName()[0];
+        });
+        dreFrontEndPatientInfo.getPatientId().then(function (patientId) {
+            dreFrontendProcedures.getByPatientId(patientId).then(function (results) {
+                $scope.model.proceduresList = [];
+                _.forEach(results.entry, function (entry) {
+                    if (angular.isObject(entry.type) && angular.isArray(entry.type.coding) && entry.type.coding.length > 0) {
+                        $scope.model.proceduresList.push({
+                            rawEntry: entry,
+                            type: entry.resourceType,
+                            title: entry.type.coding[0].display,
+                            additionalInfo: '',
+                            startDate: entry.performedDateTime,
+                            endDate: undefined
+                        })
+                    }
+                });
+            });
+        });
+        var formatDay
+    });
