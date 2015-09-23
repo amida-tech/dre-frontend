a2416f7f90c06434f724f0aa46105e10b5317c01
Igor Ivanov <iivanov@sinergo.ru>
2015-09-23 20:39:27 +0600

    fixes

--- app/scripts/controllers/matches.js
+++ app/scripts/controllers/matches.js
@@ -8,13 +8,24 @@
  * Controller of the dreFrontendApp
  */
 angular.module('dreFrontendApp')
-    .controller('MatchesCtrl', function ($http, $scope, $log) {
-        return $http({
+    .controller('MatchesCtrl', function ($http, $scope, dreFrontEndPatientInfo) {
+        $scope.model = {
+            matches: [],
+            userName: '-'
+        };
+
+        dreFrontEndPatientInfo.getPatientData().then(function (patient) {
+            $scope.model.userName = patient.getName()[0];
+        });
+
+        $http({
             url: /*'mock/diff/m-merge',*/
             /* 'mock/diff/p-merge', */
             'mock/diff/b-0',
             method: 'GET'
         }).then(function (resp) {
-            $scope.model = {matches: resp.data};
+
+            $scope.model.matches = resp.data;
         });
+
     });
