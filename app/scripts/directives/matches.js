4c7a3a1b47c25df94e2427badcf82ca35736667e
Igor Ivanov <iivanov@sinergo.ru>
2015-09-24 15:05:15 +0600

    matches fix

--- app/scripts/directives/matches.js
+++ app/scripts/directives/matches.js
@@ -7,35 +7,47 @@
  */
 
 angular.module('dreFrontendApp')
-    .directive('matches', function () {
+    .directive('matches', function (_, dreFrontendUtil, $log) {
         return {
             templateUrl: 'views/directives/matches.html',
             restrict: 'AE',
             scope: {
                 matches: "="
             },
-            controller: function ($scope, _,dreFrontendUtil, $log) {
-                var matches=[];
+            link: function(scope, element, attrs, ctrl) {
+                scope.$watch('matches', function(newValue, oldValue) {
+                    if (newValue)
+                        ctrl.format_matches(newValue);
+                }, true);
+            },
+            controller: function ($scope) {
+                this.format_matches = function(src_matches) {
+                    var matches=[];
 
-                if ($scope.matches.hasOwnProperty("changeType")) {
-                    matches.push({
-                        resourceType:"unknown",
-                        matchArray: [$scope.matches]
-                    });
-                } else {
-                    angular.forEach($scope.matches, function(res_body, res_type){
-                       if (res_type && dreFrontendUtil.isFhirResource(res_type) && res_body[0].hasOwnProperty("changeType")) {
-                           matches.push({
-                               resourceType: res_type,
-                               matchArray: res_body
-                           })
-                       }
-                    });
-                }
+                    if ($scope.matches) {
+                        if ($scope.matches.hasOwnProperty("changeType")) {
+                            matches.push({
+                                resourceType: "unknown",
+                                matchArray: [src_matches]
+                            });
+                        } else {
+                            angular.forEach($scope.matches, function (res_body, res_type) {
+                                if (res_type && dreFrontendUtil.isFhirResource(res_type) && res_body[0].hasOwnProperty("changeType")) {
+                                    matches.push({
+                                        resourceType: res_type,
+                                        matchArray: res_body
+                                    })
+                                }
+                            });
+                        }
+                    }
 
-                $scope.model = {
-                    matches: matches
+                    $scope.model = {
+                        matches: matches
+                    };
                 };
+
+                this.format_matches($scope.matches);
             }
         };
     });
